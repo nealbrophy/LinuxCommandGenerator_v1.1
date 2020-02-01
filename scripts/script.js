a23ad7177ed73324b54c0457d22b22a33afa157a
Neal Brophy <nbrophy@pm.me>
2020-02-01 13:42:55 +0000

    Add Jasmine files, implement Icon buttons, add to script.
    Jasmine files added but not yet edited.
    Buttons added to Icons list items.
    Added to if statements to OS and app/icon click listeners.

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,61 @@
 // GLOBAL VARs
-let usersOS; // to hold user's OS selection
-let appSelection; // to hold user's app selection
-let choice = "";
+let userOS; // to hold user's OS.
+let selection; // to hold user's selection.
+const choice = [];
 
 // EVENT LISTENERS
-// add click listener to OS buttons
+// add click listener to OS buttons.
 let distros = document.getElementsByClassName("distro");
 for (i = 0; i < distros.length; i++) {
     distros[i].addEventListener("click", function() {
-        usersOS = (this.id);
-        choice += usersOS;
+        document.getElementById("textAreaDump").innerText = "";
+
+        // check if OS variable is already populated, if so empty it (and remove from choice arr).
+        if (userOS) {
+         userOS = "";
+         choice.shift();
+        }
+
+        // if user clicks "Not Sure", output instructions.
+        if (this.id === "notSure") {
+            document.getElementById("textAreaDump").innerText = '$ cat /etc/issue';
+
+        // if user clicks valid OS, populate userOS var and add to choice arr.
+        } else if (!userOS && this.id !== "notSure") { 
+            userOS = this.id;
+            choice.unshift(userOS);
+            console.log(choice);
+        }
     });
 }
-// add clicks listener to app buttons
-let apps = document.getElementsByClassName("applications");
-for (i = 0; i < apps.length; i++) {
-    apps[i].addEventListener("click", function() {
-        if (usersOS) {
-            appSelection = (this.id);
-            choice += "." + appSelection;
-            console.log(choice);
-        } else {
+
+// add click listener to App buttons.
+let toInstall = document.getElementsByClassName("select");
+for (i = 0; i < toInstall.length; i++) {
+    toInstall[i].addEventListener("click", function() {
+
+        // check if selection var is already populated, if so empty it and remove from choice arr
+        if (selection) {
+            selection = "";
+            choice.pop();
+            }
+
+        // check if valid OS has been selected, if not print warning
+        if (!userOS || userOS === "NotSure") {
             console.log("Please select an OS");
+
+            // check if app var is already populated and if so empty it (and remove from choice arr)
+        } else {
+            selection = (this.id);
+            choice.push(selection);
+            console.log(choice);
         }
-        
     });
 }
 
 // OPERATING SYSTEMS
 
-// ubuntu object
+// ubuntu obj
 const ubuntu = {
     appImageLauncher: "wget https://github.com/AppImage/appimaged/releases && sudo dpkg -i appimaged_*.deb && systemctl --user add-wants default.target appimaged && systemctl --user start appimaged",
     firefox: "wget https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US && cd ~/Downloads/ && tar xjf firefox-*.tar.bz2 && sudo rm -r /opt/firefox && sudo mv firefox /opt/firefox72",
@@ -37,5 +63,13 @@ const ubuntu = {
     standardNotes: "wget https://github.com/standardnotes/desktop/releases/download/v3.0.25/Standard-Notes-3.0.25.AppImage && mv ~/Downloads/Standard-Notes-3.0.25.AppImage ~/Standard-Notes-3.0.25.AppImage && cd ~ && chmod a+x Standard-Notes-3.0.25.AppImage"
 };
 
+// debian obj
+const debian = {};
+
+// solus obj
+const solus = {};
+
+// elementary obj
+const elementary = {};
 
 
